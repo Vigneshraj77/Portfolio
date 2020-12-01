84295210e904ccb14e686c326387c2d06d049a28
Vigneshraj77 <chitraselva33@gmail.com>
2020-12-01 20:04:36 +0530

    Updates

---
+++ precache-manifest.331e84d5a46cce2ea5080ff230a01dd7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99d184294cad9d355a44ae0f114a370",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "c29223c44393c4c0d84f",
+    "url": "/Portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "5103bbd8dba43682bb29",
+    "url": "/Portfolio/static/js/2.b346def3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.b346def3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c29223c44393c4c0d84f",
+    "url": "/Portfolio/static/js/main.d0ffde9c.chunk.js"
+  },
+  {
+    "revision": "b4f6ea8748b4b447c1e0",
+    "url": "/Portfolio/static/js/runtime-main.d4d59c43.js"
+  },
+  {
+    "revision": "2714ac44ee05d59ad18fa5456ab493cf",
+    "url": "/Portfolio/static/media/calculator.2714ac44.jpg"
+  },
+  {
+    "revision": "3c852be9519a31a693cd4837fb670851",
+    "url": "/Portfolio/static/media/fall1.3c852be9.jpeg"
+  },
+  {
+    "revision": "2ea8c99d8e2ab95cb3234644219c15ef",
+    "url": "/Portfolio/static/media/propic.2ea8c99d.jpg"
+  }
+]);
